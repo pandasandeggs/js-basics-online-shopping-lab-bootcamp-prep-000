41a624c85f2e309dcdb4eed45907921fe55d7660
Chantal Wallace <theteatreehouse@gmail.com>
2018-05-18 18:02:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
   var newObject = [
-   {itemName:"bananas", itemPrice: math.floor(math.random()*100)},
-   {itemName:"pancake batter",itemPrice: math.floor(math.random()*100)},
-   {itemName:"eggs", itemPrice: math.floor(math.random()*100) },]
+   {itemName: item, itemPrice: math.floor(math.random()*100)},]
    return `<itemName> has been added to your cart.`;
 }
 
